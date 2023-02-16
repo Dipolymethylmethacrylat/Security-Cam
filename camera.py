06702dd53e84d51de6e00ab1c1d686e13d762d85
PartTime-Programmer <thomas.htl@proton.me>
2023-02-16 13:59:44 +0100

    added Browser integration

---
+++ camera.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy
+from flask import Flask, render_template, Response, stream_with_context, request
+from random import randrange
+
+face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+i = 0
+video = cv2.VideoCapture(0)
+app = Flask('__name__')
+
+
+
+def video_stream():
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+        else:
+            successful_frame_read, frame = video.read()
+            grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            face_coord = face_data.detectMultiScale(grayscale_img)
+
+            for ((x, y, w, h)) in face_coord:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (randrange(128, 255), randrange(128, 255), randrange(128, 255)), 5)
+                i = 0
+                if(str(face_coord.all) > str(0)):
+                   i = i + 1
+                   print(i)
+
+            ret, buffer = cv2.imencode('.jpeg',frame)
+            frame = buffer.tobytes()
+            yield (b' --frame\r\n' b'Content-type: imgae/jpeg\r\n\r\n' + frame +b'\r\n')
+
+
+@app.route('/camera')
+def camera():
+    return render_template('camera.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+app.run(host='0.0.0.0', port='5000', debug=False)
